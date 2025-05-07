f7434adadf6a08201393f1bdea0446d9a73cb0b4
Miguel <miguel.mendoza8602@gmail.com>
2025-05-07 17:18:05 -0500

    finished product

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ let bottomPipeImg;
 
 //physics
 let velocityX = -2;
+let velocityY = 0;
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -58,27 +63,61 @@ window.onload = function () {
   requestAnimationFrame(update);
 
   setInterval(placePipes, 1500);
+  document.addEventListener("keydown", moveBird);
 };
 
 function update() {
   requestAnimationFrame(update);
+  if (gameOver) {
+    return;
+  }
   context.clearRect(0, 0, board.width, board.height);
 
   //bird
+  velocityY += gravity;
+  bird.y = Math.max(bird.y + velocityY, 0);
   context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+  if (bird.y > board.height) {
+    gameOver = true;
+  }
 
   //pipes
   for (let i = 0; i < pipeArr.length; i++) {
     let pipe = pipeArr[i];
     pipe.x += velocityX;
     context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5;
+      pipe.passed = true;
+    }
+
+    if (detectCollision(bird, pipe)) {
+      gameOver = true;
+    }
+  }
+
+  //clear pipes
+  while (pipeArr.length > 0 && pipeArr[0].x < -pipeWidth) {
+    pipeArr.shift();
+  }
+
+  //score
+  context.fillStlye = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+
+  if (gameOver) {
+    context.fillText("GAME OVER", 5, 90);
   }
 }
 
 function placePipes() {
-  //0-1) * pipeHeight/2
-  //0 -> -128 (pipeHeight/4)
-  //1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
+  if (gameOver) {
+    return;
+  }
+
   let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
   let openingSpace = board.height / 4;
 
@@ -101,4 +140,27 @@ function placePipes() {
     passed: false,
   };
   pipeArr.push(bottomPipe);
+}
+
+function moveBird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    velocityY = -6;
+
+    //reset game
+    if (gameOver) {
+      bird.y = birdY;
+      pipeArr = [];
+      score = 0;
+      gameOver = false;
+    }
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
 }
